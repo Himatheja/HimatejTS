341c4b02ce9ed9f741aa12e634d79f669fdb81d6
Mouni <mounishmanju789@gmail.com>
2022-04-28 18:38:47 +0530

    My First Commit

---
+++ App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ fontWeight: 'bold', fontSize: 24 }}>Welcome to TS Learning..!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
